80ef9b56b04ee59527b52013c502e043b3d804f4
joshyeom <105287510+joshyeom@users.noreply.github.com>
2022-06-27 00:14:54 +0900

    #2.5 Arrays__06/27
    H Array!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-Boolean; // true and false
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
 
-const amIFat = true; // "true" is string
-console.log(amIFat); // true
+const daysOfWeek = mon + tue + wed + thu + fri + sat + sun;
+console.log(daysOfWeek); //montuewedthufrisatsun
+//이렇게 정의하면 매우 귀찮을 뿐더러 원하는 요소를 끄집어내지 못함
 
-const nothing = null;
-console.log(nothing); //null is nothing.
+const daysOfWeekArrays = [mon, tue, wed, thu, fri, sat, sun];
+console.log(daysOfWeekArrays);
+//(7) ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+//[]안에 ,로 분리
 
-let something;
-console.log(something); // undefined is 'there's no value'
-console.log(something, nothing); //undefined    null
+// Get Item from Array
+console.log(daysOfWeekArrays[4]);
+//fri
+//모든 순서는 0부터시작 그러므로 4는 5번째 fri
+
+// Add one more day to the array
+daysOfWeekArrays.push("sunsun");
+// (8) ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun', 'sunsun']
