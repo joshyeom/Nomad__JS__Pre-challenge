ea552c2cf2a72c58a4e906035300fc53463452b7
joshyeom <105287510+joshyeom@users.noreply.github.com>
2022-06-26 23:50:41 +0900

    #2.4 Booleans__06/26
    amIFat = true

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
-///console.log(); >> 변수창출, 출력
-console.log(5 + 2); //=7
+Boolean; // true and false
 
-//veriable 생성
-const a = 5;
-const b = 2;
-console.log(a + b); //=7
+const amIFat = true; // "true" is string
+console.log(amIFat); // true
 
-//Other veriable
-let c = 5;
-var d = 2;
-//always const      재선언,재할당 금지
-//sometimes let     재선언 금지, 재할당 가능
-//never var         재선언 가능, 재할당 가능
+const nothing = null;
+console.log(nothing); //null is nothing.
 
-let a = 5; // 위 const로 인해 불가능
-let c = 8; // 위 let 먼저 선언으로 인해 불가능
-c = 7; // let이기 때문에 재할당 가능.
-var d = 3; //var은 재선언 가능
-d = 6; //var은 재할당 가능
-
-//camelCase
-const veryVeryVeryLongName = 7;
-console.log(veryVeryVeryLongName); //=7
+let something;
+console.log(something); // undefined is 'there's no value'
+console.log(something, nothing); //undefined    null
