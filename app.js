7a3516094681d66f958c50675c2954b161a9215a
joshyeom <105287510+joshyeom@users.noreply.github.com>
2022-06-29 13:44:05 +0900

    #2.7~#2.8 Functions__06/29
    function something(argument) {
       console.log("hello" + argument);
    }
    something("blah");
    //hello blah

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
-//objects
+//function 계속 반복해서 사용할 수 있는 코드 조각
+function sayHello() {
+  console.log("Hello!");
+}
+sayHello();
+//Hello!
+//실행버튼만 누른 것
+
+function personInfo(nameOfPerson, age) {
+  console.log(nameOfPerson);
+}
+personInfo("nico", 10);
+//nico, 10
+//personOfInfo function을 만들고 nameOfPerson과 age라는 argument를 갖고있다.
+
+function selfIntroduce(nameOfPerson, age) {
+  console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+}
+selfIntroduce("nico", 10);
+//Hello my name is nico and I'm 10
+
+///////////Let's make some calculatores!//////
+function plus(a, b) {
+  console.log(a + b);
+}
+plus(8, 60);
+//68
+function divide(a, b) {
+  console.log(a / b);
+}
+divide(8, 4);
+//2
+
+//const먼저 하고 function
 const player = {
   name: "nico",
-  points: 10,
-  fat: true,
+  sayHi: function (otherPersonsName) {
+    console.log("hi! " + otherPersonsName + " nice to meet you!");
+  },
 };
-//선언 = {property: value,}
+console.log(player.name);
+player.sayHi("lynn");
+//hi! lynn nice to meet you!
 
-console.log(player);
-//fat:true
-player.fat = false;
-console.log(player);
-//fat:false
-//object 안에 내용을 수정한 것이기 때문에 const에 영향을 받지 아니함.
-player = false;
-//이 경우가 에러
-player.lastName = "potato"; //push!
-console.log(player);
-//{name: 'nico', points: 10, fat: false, lastName: 'potato'}
-player.points = player.points + 15;
-console.log(player.points);
-//25
+//()은 실행코드
+//
