20bc7da51e9987f932a1bb966aee29007e668529
joshyeom <105287510+joshyeom@users.noreply.github.com>
2022-07-01 17:01:05 +0900

    #3.5 More Events__07/01
    오우 재밌다!

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-  console.log("title was clicked");
+  h1.innerText = "mouse is here!";
 }
 
-title.addEventListener("click", handleTitleClick); //(1)title was clicked
-//function을 추가함으로써, event listner가 click을 감지했을때 handleTitleClick을 자바 스크립트가 실행.
-//즉, 나는 직접적으로 function을 실행하지 않고 java script에게 넘겨줌으로써 구현이됨.
+h1.onmouseenter = handleTitleClick;
+//addEventListner를 사용하지않고 이렇게도 사용할 수 있다.
+//그러나, addEventListner를 사용하는것이 더 가독성이 좋고 후에 .removeEvnetLisnter를 사용할 수 있으므로, addEventListner를 사용하자!
 
-function handeleTitleColor() {
-  title.style.color = "blue";
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
 }
+window.addEventListener("resize", handleWindowResize);
+//브라우저 사이즈를 늘이거나 줄이면 tomato로 바뀜.
+//console에서 document.body를 입력하면 바뀐값이 출력됨.
 
-title.addEventListener("click", handeleTitleColor); //클릭시 파랑색으로 변경됨.
-//addEventListnere명령시 무엇을 listen 할 지 적용해주어야 함.
-//listner안에 함수에는 ()을 넣으면 안된다. 우리는 함수만 넘겨주고 자바스크립트가 실행하게끔 해야한다.
+function handleWindowCopy() {
+  alert("copier!");
+}
+window.addEventListener("copy", handleWindowCopy); //copy시 알람
+
+function handleWindowOffline() {
+  alert("Check your WI-FI");
+}
+window.addEventListener("offline", handleWindowOffline); //오프라인 시 알람
