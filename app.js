467efa64ac3f1f0a29b7710814e6f1c5c36ca118
joshyeom <105287510+joshyeom@users.noreply.github.com>
2022-07-01 15:52:26 +0900

    #3.3 Events__07/01
    야야야 JS야 엣다 이것좀 실행해라

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-//Uncaught TypeError: Cannot set properties of null (setting 'innerText')
-//코드내의 어떤 값이 null이라는 에러
-const title = document.getElementsByTagName("h1");
-console.log(title); //HTMLCollection [h1]0: h1length: 1[[Prototype]]: HTMLCollection
+const title = document.querySelector(".hello h1");
 
-const selector = document.querySelector(".hello h1");
-console.log(selector); //<h1>Grab me!</h1>
-//querySelector로 보다 펀하고 쿨하고 섹시하게 파일값을 출력할 수 있다.
-//중복되는 태그가 있다면 첫 번째 요소만 가져온다.
-//다 가져오고 싶으면 .querySelectorAll 을 사용
+function handleTitleClick() {
+  console.log("title was clicked");
+}
 
-const selector = document.getElementsByClassName("hello");
-const selector = document.querySelector(".hello");
-//이 두 코드의 출력값은 같다.
+title.addEventListener("click", handleTitleClick); //(1)title was clicked
+//function을 추가함으로써, event listner가 click을 감지했을때 handleTitleClick을 자바 스크립트가 실행.
+//즉, 나는 직접적으로 function을 실행하지 않고 java script에게 넘겨줌으로써 구현이됨.
 
-const selector = document.getElementById("hello");
-const selector = document.querySelector("#hello");
-//이 두개도 같다.
-//하지만, querySelector만 하위요소를 가져올 수 있어 이것이 더 펀쿨섹이다.
+function handeleTitleColor() {
+  title.style.color = "blue";
+}
+
+title.addEventListener("click", handeleTitleColor); //클릭시 파랑색으로 변경됨.
+//addEventListnere명령시 무엇을 listen 할 지 적용해주어야 함.
+//listner안에 함수에는 ()을 넣으면 안된다. 우리는 함수만 넘겨주고 자바스크립트가 실행하게끔 해야한다.
