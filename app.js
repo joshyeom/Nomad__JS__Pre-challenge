d38175f75729bd6ea020be47fc6377ea93893d20
joshyeom <105287510+joshyeom@users.noreply.github.com>
2022-07-02 13:22:30 +0900

    #3.7 CSS in Javascript__07/02
    class가 JS에서 생성되다니!!!!!!!!!

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-  const currentColor = h1.style.color; //현재상태의 색깔 값을 currentColor에 저장
-  let newColor; //newColor라는 새 변수 선언
-  if (currentColor === "blue") {
-    //currentColor가 blue인지 확인
-    newColor = "tomato"; //newColor = "tomato"라는 데이터값이 저장
-  } else {
-    newColor = "blue"; //currentColor === "blue"가 false 일경우 newColor는 blue
-  }
-  h1.style.color = newColor; //newColor는 if문에서 tomato를 할당받았기 때문에 newColor = "tomato" = h1.style.color
-  //h1.style.color = "tomato" = currentColor
-  //if에서 currentColor는 "tomato"라 false
-  //else에 따라 newColor="blue"
-  //그러므로 처음 값은 blue
+  h1.className = "active"; //css에서 입력된 class 수정 가능
 }
-
 h1.addEventListener("click", handleTitleClick); //첫 클릭은 blue 두 번째 클릭은 tomato
+
+const h2 = document.querySelector(".hello h2");
+
+function switchColor() {
+  if (h2.className === "active") {
+    //h2.className이 active일 시
+    h2.className = ""; //h2.className = ""이 되는거고
+  } else {
+    //아니면
+    h2.className = "active"; //h2.className = "active"이다.
+  } //그렇다면 다시 처음부터 시작
+} //그렇지만 함수안에 "action" 스트링이 2개여서 실수 및 에러의 위험이 높다.
+
+h2.addEventListener("click", switchColor); //console에서 class("active")사라졌다가 나타났다를 반복 하는것을 볼 수 있다.
