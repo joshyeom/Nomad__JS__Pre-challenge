1b94bc4702f817755c816acad50a0f7166dad1e1
joshyeom <105287510+joshyeom@users.noreply.github.com>
2022-07-04 14:36:47 +0900

    #4.2 Events__07/04
    어렵지만 잼지만 어렵지만 잼다

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
+function onLoginSubmit() {
   const username = loginInput.value;
   console.log("username");
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+//form에는 submit할때 자동으로 refresh되는 기능이 있다.
+//여기까지는 value값을 받도록 코딩되어 있지만, 받자마자 refresh 되어버림
+//그래서 우리는 이 기능을 초기화를 해야함
+
+function onLoginSubmit(event) {
+  //event라는 arugment를 object로 생성
+  event.preventDefault(); //브라우저가 기본 동작을 실행하지 못하게 막기
+  console.log(loginInput.value);
+}
