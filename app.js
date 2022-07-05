095fa1cf201e774dcbad04579d454628c357b6af
joshyeom <105287510+joshyeom@users.noreply.github.com>
2022-07-05 14:57:18 +0900

    #4.6 Loading Username__07/05
    어렵다...
    21 const savedUsername = localStorage.getItem("USERNAME_KEY")로해서 한참해멤

--- app.js
+++ app.js
@@ -3,17 +3,26 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
-
+const USERNAME_KEY = "username"; //username string can make mistake. You should set new veriable.
+//변수로 저장시 오류 찾기도 유효!
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", josh); //storage에 username(key) josh(value)를 저장
-  localStorage.getItem("username"); //불러오기
-  localStorage.removeItem("username", josh); //제거하기
-  //마찬가지로 form에 입력한 value도 storage에 저장됨.
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
