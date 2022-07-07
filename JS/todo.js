201e76159eb5a3ebfb512667dca46075050adbef
joshyeom <105287510+joshyeom@users.noreply.github.com>
2022-07-07 20:50:02 +0900

    #7 ToDo-List__07/07
    어찌저찌 이해는 했는데 내가 하라고 하면 못할꺼 같다 쓋!

--- JS/todo.js
+++ JS/todo.js
@@ -4,22 +4,25 @@ const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
 
-const toDos = []; //toDos >> array
+let toDos = []; //toDos >> array //빈 array로 지정되어있어서 refresh하게 되면 기록들이 사라짐
+//정확히는 사라진다는 것보다, 빈 상태로 시작. 그래서 나중에 재할당이 가능하게끔 let으로 바꿔줌.
 
 function saveToDos() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); //JSON.stringify는 어떠한 코드든 string으로 변환 또한, string으로 되어서 중복적용도 가능.
 }
 
 function deltedToDo(event) {
-  //event를 넣지 않으면 모든 eventListner가 같은 명령을 기다리기 때문에 함수가 실행되면 다 지워짐. 이를 방지하기 위해 event로 각 객체의 정보를 받음
-  const li = event.target.parentElement; //event의 정보중 target으로 button이 target인것을 알려주고 button의 parent를 확인함으로써 click시 객체를 제대로 알려줌
+  const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 function paintToDo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span"); //li안에 span이 생성되는 것이 아니기 때문에
-  span.innerText = newTodo; //newTodo의 value를 span으로 바꿔서 텍스트로 넣어주기
+  span.innerText = newTodo.text; //newTodo의 value를 span으로 바꿔서 텍스트로 넣어주기
   const button = document.createElement("button");
   button.innerText = "❌";
   button.addEventListener("click", deltedToDo);
@@ -28,21 +31,27 @@ function paintToDo(newTodo) {
   toDoList.appendChild(li); //ul에 appendChild로 넣어주기
 }
 
-function handleToDoSubmint(event) {
+function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value; //input의 현재 value를 새로운 변수에 복사
   toDoInput.value = "";
-  toDos.push(newTodo); //새로운 value를 toDos array에 push.
-  paintToDo(newTodo);
+  const newTodoObj = {
+    //오브젝트로 저장되게끔 선언
+    text: newTodo,
+    id: Date.now(),
+  }; //Date.now는 지금 현재 정확한 미세 초 단위까지 출력해주기 때문에, 랜덤하고 고유한 id가 필요할때 적합하다.
+  toDos.push(newTodoObj); //새로운 value를 toDos array에 push.
+  paintToDo(newTodoObj);
   saveToDos();
 }
 
-toDoForm.addEventListener("submit", handleToDoSubmint);
+toDoForm.addEventListener("submit", handleToDoSubmit);
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if (savedToDos !== null) {
   //일치 하지 않다면
   const parsedToDos = JSON.parse(savedToDos); //string을 object로 바꿔줌
-  parsedToDos.forEach((item) => console.log("this is the turn of", item)); //각각의 item을 실행해줌.
+  toDos = parsedToDos; //위에 toDos는 let으로 선언되었기 때문에 재할당 가능.
+  parsedToDos.forEach(paintToDo); //각각의 item을 실행해줌.
 }
